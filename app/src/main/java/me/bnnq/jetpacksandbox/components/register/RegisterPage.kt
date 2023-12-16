5d4d9aa0cd71ec271bd0fae60d2939922ab2bd11
BnnQ <1v25fa94@gmail.com>
2023-12-16 12:51:30 +0200

    Added register page with viewmodel

---
+++ app/src/main/java/me/bnnq/jetpacksandbox/components/register/RegisterPage.kt
@@ -0,0 +1,108 @@
+package me.bnnq.jetpacksandbox.components.register
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import me.bnnq.jetpacksandbox.R
+
+@Composable
+fun RegisterPage(
+    context: Context,
+    navController: NavController,
+    viewModel: RegisterViewModel = hiltViewModel())
+{
+    val registerResult by viewModel.registerResult.collectAsState()
+    val registerErrorMessage by viewModel.registerErrorMessage.collectAsState()
+
+    when (registerResult)
+    {
+        true -> navController.navigate("conversations")
+        false ->
+        {
+            Toast.makeText(context, registerErrorMessage, Toast.LENGTH_SHORT).show()
+        }
+
+        null ->
+        {
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Card(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp)
+                .shadow(30.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 20.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Register", style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = viewModel.getUsername(),
+                    onValueChange = { viewModel.setUsername(it) },
+                    label = { Text("Username") },
+                    maxLines = 3,
+                    isError = viewModel.usernameValidationError != null,
+                    supportingText = { Text(viewModel.usernameValidationError ?: "") })
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = viewModel.getPassword(),
+                    onValueChange = { viewModel.setPassword(it) },
+                    label = { Text("Password") },
+                    maxLines = 3,
+                    isError = viewModel.passwordValidationError != null,
+                    supportingText = { Text(viewModel.passwordValidationError ?: "") })
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = viewModel.getConfirmPassword(),
+                    onValueChange = { viewModel.setConfirmPassword(it) },
+                    label = { Text("Confirm Password") },
+                    maxLines = 3,
+                    isError = viewModel.confirmationPasswordValidationError != null,
+                    supportingText = { Text(viewModel.confirmationPasswordValidationError ?: "") })
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { viewModel.register() }) {
+                    Text("REGISTER")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/me/bnnq/jetpacksandbox/components/register/RegisterViewModel.kt
@@ -0,0 +1,62 @@
+package me.bnnq.jetpacksandbox.components.register
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import me.bnnq.jetpacksandbox.services.abstractions.IAuthenticationService
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val authenticationService : IAuthenticationService) : ViewModel() {
+    private val username = mutableStateOf("")
+    private val password = mutableStateOf("")
+    private val confirmPassword = mutableStateOf("")
+
+    fun getUsername() = username.value
+    fun getPassword() = password.value
+    fun getConfirmPassword() = confirmPassword.value
+
+    fun setUsername(newUsername: String) {
+        viewModelScope.launch {
+            username.value = newUsername
+        }
+    }
+
+    fun setPassword(newPassword: String) {
+        viewModelScope.launch {
+            password.value = newPassword
+        }
+    }
+
+    fun setConfirmPassword(newConfirmPassword: String) {
+        viewModelScope.launch {
+            confirmPassword.value = newConfirmPassword
+        }
+    }
+
+    val registerErrorMessage: MutableStateFlow<String?> = MutableStateFlow(null)
+    val registerResult: MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    var usernameValidationError by mutableStateOf<String?>(null)
+    var passwordValidationError by mutableStateOf<String?>(null)
+    var confirmationPasswordValidationError by mutableStateOf<String?>(null)
+
+    fun register() {
+        usernameValidationError = if(username.value.isEmpty()) "Username cannot be empty" else null
+        passwordValidationError = if(password.value.isEmpty()) "Password cannot be empty" else null
+        confirmationPasswordValidationError = if(confirmPassword.value.isEmpty() || confirmPassword.value != password.value) "Confirmation password cannot be empty" else null
+
+        if (usernameValidationError != null || passwordValidationError != null || confirmationPasswordValidationError != null) {
+            return
+        }
+
+        viewModelScope.launch {
+            registerResult.value = authenticationService.register(username.value, password.value).success
+        }
+    }
+
+}
