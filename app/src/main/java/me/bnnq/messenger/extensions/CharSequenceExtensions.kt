82b83656426b387be75195abecd115a9deca19e4
BnnQ <1v25fa94@gmail.com>
2023-12-24 11:39:15 +0200

    Removed unnecessary char sequence extensions

--- app/src/main/java/me/bnnq/messenger/extensions/CharSequenceExtensions.kt
+++
@@ -1,5 +0,0 @@
-package me.bnnq.messenger.extensions
-
-fun CharSequence.isWhitespace(): Boolean = this.all { it.isWhitespace() }
-
-fun CharSequence.isEmptyOrWhitespace(): Boolean = this.isWhitespace() || this.isEmpty()
