baf8352e08eba1005229034303adea4391c45095
BnnQ <1v25fa94@gmail.com>
2023-12-24 11:44:05 +0200

    Added ChatPage with viewmodel

--- app/src/main/java/me/bnnq/messenger/components/chat/ChatPage.kt
+++ app/src/main/java/me/bnnq/messenger/components/chat/ChatPage.kt
@@ -1,6 +1,9 @@
 package me.bnnq.messenger.components.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,45 +12,67 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import me.bnnq.messenger.R
 import me.bnnq.messenger.models.Message
+import me.bnnq.messenger.services.ServerCommunicationPool
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatPage(
+    navController: NavController,
     viewModel: ChatViewModel
 )
 {
+    DisposableEffect(viewModel) {
+        viewModel.onInit()
+        onDispose { viewModel.onDispose() }
+    }
+
     val messages by viewModel.messages.collectAsState()
     val messageListScrollState = rememberLazyListState()
-
-    // Load messages when ChatPage appears
-    LaunchedEffect(viewModel.chatId)
-    {
-        viewModel.loadMessages()
-    }
 
     // Scroll to the bottom when messages are loaded / new messages added
     LaunchedEffect(key1 = messages)
@@ -56,36 +81,79 @@ fun ChatPage(
             messageListScrollState.animateScrollToItem(messages.size - 1)
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        LazyColumn(modifier = Modifier.weight(1f), state = messageListScrollState) {
-            items(messages) { message ->
-                MessageCard(message, viewModel.currentUserId == message.senderId)
-                Spacer(modifier = Modifier.height(1.dp))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(viewModel.chatAvatar.value)
+                                .crossfade(true)
+                                .build(),
+                            placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                            contentDescription = "Avatar",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .width(38.dp)
+                                .height(38.dp)
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Column {
+                            Text(viewModel.chatName.value)
+                            val isUserOnline by viewModel.isUserOnline.collectAsState()
+                            Text(text = if (isUserOnline) "Online" else "Offline", fontSize = 10.sp)
+                        }
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it))
+            {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                    //.padding(16.dp)
+                ) {
+
+                    LazyColumn(modifier = Modifier.weight(1f), state = messageListScrollState) {
+                        items(messages) { message ->
+                            MessageCard(message, ServerCommunicationPool.currentUser!!.id == message.senderId)
+                            Spacer(modifier = Modifier.height(1.dp))
+                        }
+                    }
+
+                    OutlinedTextField(
+                        value = viewModel.getMessage(),
+                        onValueChange = { viewModel.setMessage(it) },
+                        label = { Text("Message...") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+
+                    Button(
+                        onClick = { viewModel.sendMessage() },
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .padding(8.dp)
+                    ) {
+                        Text("Send")
+                    }
+                }
             }
         }
-
-        OutlinedTextField(
-            value = viewModel.getMessage(),
-            onValueChange = { viewModel.setMessage(it) },
-            label = { Text("Message...") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-
-        Button(
-            onClick = { viewModel.sendMessage() },
-            modifier = Modifier
-                .align(Alignment.End)
-                .padding(8.dp)
-        ) {
-            Text("Send")
-        }
-    }
+    )
 }
 
 @Composable
@@ -109,7 +177,7 @@ fun MessageCard(message: Message, fromCurrentUser: Boolean)
                     modifier = Modifier.padding(8.dp)
                 ) {
                     Text(
-                        text = message.content,
+                        text = message.text,
                         style = MaterialTheme.typography.bodyMedium.copy(
                             fontSize = TextUnit(17f, TextUnitType.Sp)
                         )
--- app/src/main/java/me/bnnq/messenger/components/chat/ChatViewModel.kt
+++ app/src/main/java/me/bnnq/messenger/components/chat/ChatViewModel.kt
@@ -2,31 +2,135 @@ package me.bnnq.messenger.components.chat
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.GsonBuilder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import me.bnnq.messenger.enums.ActionType
+import me.bnnq.messenger.extensions.fromJson
 import me.bnnq.messenger.models.Message
-import me.bnnq.messenger.models.Result
-import me.bnnq.messenger.services.abstractions.IAuthenticationService
+import me.bnnq.messenger.models.dto.UserInfoDto
+import me.bnnq.messenger.models.dto.UserStatusDto
+import me.bnnq.messenger.models.eventargs.ServerUpdateEventArgs
+import me.bnnq.messenger.services.ServerCommunicationPool
+import me.bnnq.messenger.services.abstractions.IChatRepository
 import me.bnnq.messenger.services.abstractions.IMessageRepository
+import me.bnnq.messenger.services.abstractions.ViewModelBase
 import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     private val messageRepository: IMessageRepository,
-    private val authenticationService: IAuthenticationService,
+    private val chatRepository: IChatRepository,
     savedStateHandle: SavedStateHandle
-) : ViewModel()
+) : ViewModelBase()
 {
+    val chatName = mutableStateOf("")
+    val chatAvatar = mutableStateOf("")
+    val otherUserId = mutableStateOf<String?>(null)
+
+    private val onChatUpdated = fun(eventArgs: ServerUpdateEventArgs)
+    {
+        if (eventArgs.actionType == ActionType.ChatMessagesUpdated)
+        {
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create()
+            _messages.value = gson.fromJson<List<Message>>(eventArgs.actionData!!.toString())
+        }
+    }
+
+    private val onMessageSent = fun(eventArgs: ServerUpdateEventArgs)
+    {
+        if (eventArgs.actionType == ActionType.MessageSent)
+        {
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create()
+            _messages.value = _messages.value + gson.fromJson<Message>(eventArgs.actionData!!.toString())
+        }
+    }
+
+    private val onMessageReceived = fun(eventArgs: ServerUpdateEventArgs)
+    {
+        if (eventArgs.actionType == ActionType.MessageReceived)
+        {
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create()
+            _messages.value = _messages.value + gson.fromJson<Message>(eventArgs.actionData!!.toString())
+        }
+    }
+
+    private val onChatUsersUpdated = fun(eventArgs: ServerUpdateEventArgs)
+    {
+        if (eventArgs.actionType == ActionType.ChatUsersUpdated)
+        {
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create()
+
+            val chatUsers =
+                gson.fromJson<List<UserInfoDto>>(eventArgs.actionData!!.toString())
+                    .filter { it.id != ServerCommunicationPool.currentUser!!.id }
+            val otherUser = chatUsers.first()
+            chatName.value = otherUser.username
+            chatAvatar.value = otherUser.avatarImagePath
+            otherUserId.value = otherUser.id
+        }
+    }
+
+    val isUserOnline = MutableStateFlow(false)
+    private var delayJob: Job? = null
+    private val onOtherUserStatusChanged = fun(eventArgs: ServerUpdateEventArgs)
+    {
+        if (eventArgs.actionType == ActionType.UserStatusChanged)
+        {
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create()
+
+            val userStatus = gson.fromJson<UserStatusDto>(eventArgs.actionData!!.toString())
+            if (userStatus.userId == otherUserId.value)
+            {
+                delayJob?.cancel()
+                isUserOnline.value = true
+
+                delayJob = viewModelScope.launch {
+                    delay(15000) // wait for 30 seconds
+                    isUserOnline.value = false
+                }
+            }
+        }
+    }
+
+    override fun onInit()
+    {
+        ServerCommunicationPool.actionUpdateEvent += onChatUpdated
+        ServerCommunicationPool.actionUpdateEvent += onMessageReceived
+        ServerCommunicationPool.actionUpdateEvent += onOtherUserStatusChanged
+
+        loadMessages()
+        loadChatUsers()
+    }
+
+    override fun onDispose()
+    {
+        ServerCommunicationPool.actionUpdateEvent -= onChatUpdated
+        ServerCommunicationPool.actionUpdateEvent -= onMessageReceived
+        ServerCommunicationPool.actionUpdateEvent -= onOtherUserStatusChanged
+        delayJob?.cancel()
+    }
+
     private val _messages = MutableStateFlow<List<Message>>(emptyList())
     val messages: StateFlow<List<Message>> = _messages
 
     val chatId = savedStateHandle.get<String>("chatId")?.toIntOrNull()
-    val currentUserId = authenticationService.getCurrentUser()?.id
 
     fun loadMessages()
     {
@@ -34,7 +138,18 @@ class ChatViewModel @Inject constructor(
             return
 
         viewModelScope.launch {
-            _messages.value = messageRepository.getChatMessages(chatId)
+            messageRepository.getChatMessages(chatId)
+        }
+    }
+
+    fun loadChatUsers()
+    {
+        if (chatId == null)
+            return
+
+        viewModelScope.launch {
+            val actionId = chatRepository.getChatUsers(chatId)
+            ServerCommunicationPool.actionUpdateEvent.subscribeOnceWithCondition(onChatUsersUpdated) { it.actionId == actionId }
         }
     }
 
@@ -50,31 +165,23 @@ class ChatViewModel @Inject constructor(
         }
     }
 
-    val sendMessageResult: MutableStateFlow<Result?> = MutableStateFlow(null)
-
     fun sendMessage()
     {
-        if (currentUserId != null)
+        if (ServerCommunicationPool.currentUser?.id != null)
         {
             viewModelScope.launch {
-                val sentMessage = messageRepository.sendMessage(
+                val actionId = messageRepository.sendMessage(
                     chatId!!,
                     Message(
                         chatId,
-                        currentUserId,
+                        ServerCommunicationPool.currentUser!!.id,
                         message.value,
                         Date())
                 )
 
-                if (sentMessage != null)
-                {
-                    _messages.value = _messages.value + sentMessage
-                    message.value = ""
-                    sendMessageResult.value = Result()
-                } else
-                {
-                    sendMessageResult.value = Result("Failed to send message")
-                }
+                setMessage("")
+
+                ServerCommunicationPool.actionUpdateEvent.subscribeOnceWithCondition(onMessageSent) { it.actionId == actionId }
             }
         }
     }
