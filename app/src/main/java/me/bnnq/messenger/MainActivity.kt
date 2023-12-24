f2c4c71aa8c6c72eb92648c4c7b628fe552db579
BnnQ <1v25fa94@gmail.com>
2023-12-24 11:46:01 +0200

    Updated core application

--- app/src/main/java/me/bnnq/messenger/MainActivity.kt
+++ app/src/main/java/me/bnnq/messenger/MainActivity.kt
@@ -1,12 +1,15 @@
 package me.bnnq.messenger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import me.bnnq.messenger.services.ServerCommunicationPool
 import me.bnnq.messenger.ui.theme.JetpackSandboxTheme
 import kotlin.coroutines.CoroutineContext
 
@@ -22,6 +25,18 @@ class MainActivity : ComponentActivity(), CoroutineScope
     {
         super.onCreate(savedInstanceState)
         job = Job()
+        val instance = FirebaseMessaging.getInstance()
+        instance.token.addOnCompleteListener { task ->
+            if (!task.isSuccessful)
+            {
+                Log.w("ServerCommunicationPool", "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+
+            ServerCommunicationPool.updateFcmTokenOnServer(task.result!!)
+
+            Log.d("ServerCommunicationPool", "FCM registration token: ${task.result!!}")
+        }
 
         setContent {
             JetpackSandboxTheme {
--- app/src/main/java/me/bnnq/messenger/MessengerApplication.kt
+++ app/src/main/java/me/bnnq/messenger/MessengerApplication.kt
@@ -4,5 +4,17 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MessengerApplication : Application() {
+class MessengerApplication : Application()
+{
+    companion object
+    {
+        lateinit var instance: MessengerApplication
+            private set
+    }
+
+    override fun onCreate()
+    {
+        super.onCreate()
+        instance = this
+    }
 }
